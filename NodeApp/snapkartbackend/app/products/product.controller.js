47809e52af4007547a7637488ae8641177684329
k3informatics <kiran_p@k3informatics.com>
2017-03-09 17:06:48 +0530

    added code

--- NodeApp/snapkartbackend/app/products/product.controller.js
+++ NodeApp/snapkartbackend/app/products/product.controller.js
@@ -12,4 +12,57 @@ productCtrl.postMany = function(req, res) {
     })
 };
 
+productCtrl.search = function(req, res) {
+    var criteria = req.body;
+    console.log(criteria)
+        //or query
+    productSchema.find(
+        // {
+        //     // //$in query
+        //     // "Model": {
+        //     //     $in: ["iPad", "iPhone", "Lenovo"]
+        //     // }
+        //     // // $or: [{
+        //     // //         Model: criteria.model
+        //     // //     },
+        //     // //     { Price: criteria.price }
+        //     // // ]
+        // },
+        criteria,
+        function(err, data) {
+            if (err) {
+                console.log(err);
+                res.json({ "error": "Error Occured" });
+            } else {
+                res.render("products", { products: data });
+            }
+        });
+
+
+    //search products by price
+
+    // productSchema.find({
+    //     Price: {
+    //         $gt: criteria.minprice,
+    //         $lt: criteria.maxprice
+    //     }
+    // }, function(err, data) {
+    //     if (err) {
+    //         console.log(err);
+    //         res.json({ "error": "Error Occured" });
+    //     } else {
+    //         res.json(data);
+    //     }
+    // })
+
+    // productSchema.find(criteria, function(err, data) {
+    //     if (err) {
+    //         console.log(err);
+    //         res.json({ "error": "Error Occured" });
+    //     } else {
+    //         res.json(data);
+    //     }
+    // })
+};
+
 module.exports = productCtrl;
--- NodeApp/snapkartbackend/app/products/product.route.js
+++ NodeApp/snapkartbackend/app/products/product.route.js
@@ -2,7 +2,8 @@ var productCtrl = require("./product.controller");
 
 module.exports = function(app) {
     //app.get("/products/get", productCtrl.get);
-    //app.post("/products/post", productCtrl.post);
+    app.get("/products/search", productCtrl.search);
+    app.post("/products/search", productCtrl.search);
     app.post("/products/postBulk", productCtrl.postMany);
 
 }
